60df9ec429d05fedd1c2709adf59747ea893a85d
hyq.neuron@gmail.com <hyq.neuron@gmail.com@0e4da3b9-f550-2c14-ba48-a28a6391fce2>
2012-01-31 12:45:05 +0000

    Updated case nvcc_tests/block_for

--- tests/nvcc_tests/block_for/1.asm
+++ tests/nvcc_tests/block_for/1.asm
@@ -1,23 +1,49 @@
 
-code for sm_20
-	Function : _Z1kPiS_i
-/*0000*/     MOV R1, c [0x1] [0x100];
-/*0008*/     S2R R0, SR_Tid_X;
-/*0010*/     ISETP.GE.AND P0, pt, R0, c [0x0] [0x30], pt;
-/*0018*/     @P0 EXIT;
-/*0020*/     MOV32I R8, 0x4;
-/*0028*/     IMUL.HI R3, R0, 0x4;
-/*0030*/     IMAD R6.CC, R0, R8, c [0x0] [0x20];
-/*0038*/     IADD.X R7, R3, c [0x0] [0x24];
-/*0040*/     IMAD R4.CC, R0, R8, c [0x0] [0x28];
-/*0048*/     IADD R0, R0, c [0x0] [0x8];
-/*0050*/     LD.E R2, [R6];
-/*0058*/     IADD.X R5, R3, c [0x0] [0x2c];
-/*0060*/     ISETP.LT.AND P0, pt, R0, c [0x0] [0x30], pt;
-/*0068*/     SHL R2, R2, 0x4;
-/*0070*/     ST.E [R4], R2;
-/*0078*/     @P0 BRA 0x28;
-/*0080*/     EXIT;
-	..........................
+	code for sm_20
+		Function : _Z1kPiS_i
+	/*0000*/      	MOV R1, c [0x1] [0x100];
+	/*0008*/      	S2R R8, SR_Tid_X;
+	/*0010*/      	ISETP.GE.AND P0, pt, R8, c [0x0] [0x30], pt;
+	/*0018*/      	@P0 EXIT;
+	/*0020*/      	MOV R0, c [0x0] [0x8];
+	/*0028*/      	MOV32I R7, 0x4;
+	/*0030*/      	PBK 0x150;
+	/*0038*/      	IMUL.HI R6, R0, 0x4;
+	/*0040*/      	SHL.W R0, R0, 0x2;
+	/*0048*/      	IMUL.HI R3, R8, 0x4;
+	/*0050*/      	IMAD R4.CC, R8, R7, c [0x0] [0x20];
+	/*0058*/      	IADD.X R5, R3, c [0x0] [0x24];
+	/*0060*/      	IMAD R2.CC, R8, R7, c [0x0] [0x28];
+	/*0068*/      	IADD R8, R8, c [0x0] [0x8];
+	/*0070*/      	LD.E R9, [R4];
+	/*0078*/      	IADD.X R3, R3, c [0x0] [0x2c];
+	/*0080*/      	ISETP.GE.AND P0, pt, R8, c [0x0] [0x30], pt;
+	/*0088*/      	SHL R9, R9, 0x4;
+	/*0090*/      	ST.E [R2], R9;
+	/*0098*/      	@P0 BRK;
+	/*00a0*/      	IADD R4.CC, R4, R0;
+	/*00a8*/      	IADD R10, R8, c [0x0] [0x8];
+	/*00b0*/      	IADD.X R5, R5, R6;
+	/*00b8*/      	IADD R2.CC, R2, R0;
+	/*00c0*/      	ISETP.GE.AND P0, pt, R10, c [0x0] [0x30], pt;
+	/*00c8*/      	LD.E R9, [R4];
+	/*00d0*/      	IADD.X R3, R3, R6;
+	/*00d8*/      	SHL R9, R9, 0x4;
+	/*00e0*/      	ST.E [R2], R9;
+	/*00e8*/      	@P0 BRK;
+	/*00f0*/      	IADD R4.CC, R4, R0;
+	/*00f8*/      	MOV R9, c [0x0] [0x8];
+	/*0100*/      	IADD.X R5, R5, R6;
+	/*0108*/      	IADD R10.CC, R2, R0;
+	/*0110*/      	ISCADD R8, R9, R8, 0x1;
+	/*0118*/      	LD.E R4, [R4];
+	/*0120*/      	IADD.X R11, R3, R6;
+	/*0128*/      	ISETP.LT.AND P0, pt, R8, c [0x0] [0x30], pt;
+	/*0130*/      	SHL R2, R4, 0x4;
+	/*0138*/      	ST.E [R10], R2;
+	/*0140*/      	@!P0 BRK;
+	/*0148*/      	BRA 0x48;
+	/*0150*/      	EXIT;
+		..........................
 
 
--- tests/nvcc_tests/block_for/short.asm
+++ tests/nvcc_tests/block_for/short.asm
@@ -23,42 +23,60 @@
 	IADD R4, R4, R1; //output_add loaded
 	MOV R1, c[0x0][0x8];
 	SHL R1, R1, 0x2; //row size loaded
-	ISCADD R4, -R1, R4, 1;
+	IMAD R4, -R1, 3, R4;
 	IADD R10, R2, R1;//R10,11=next2nd_addr
 	MOV R11, R3;
 	IADD R12, R4, R1;
 	MOV R13, R5;
+	IADD R14, R10, R1;
+	IADD R16, R12, R1;
+	MOV R15, R3;
+	MOV R17, R5;
 
 	ISETP.GE.AND P0, pt, R2, R0, pt; //done_m >= dolim?
 	@P0 EXIT;
 	//done_m <= dolim -2 ? do2: do1
-	ISCADD R0, -R1, R0, 1;
+	IMAD R0, -R1, 3, R0;
 	ISETP.LE.AND P0, pt, R2, R0, pt;
+	@!P0 IMAD R0, R1, 3, R0;
 	@!P0 BRA !ENDLOOP
 
 
 !Label LOOP
-	LD.E R6, [R2];
-	LD.E R7, [R10];
-	ISCADD R4, R1, R4, 1;
-	ISCADD R12,R1, R12,1;
+	LD.E.CG R6, [R2];
+	LD.E.CG R7, [R10];
+	LD.E.CG R8, [R14];
+	IMAD R4,  R1, 3, R4;
+	IMAD R12, R1, 3, R12;
+	IMAD R16, R1, 3, R16;
+	IMAD R2, R1,  3, R2;
 	SHL R6, R6, 0x4;
-	ISCADD R2, R1, R2, 1;
 	SHL R7, R7, 0x4;
-	ISCADD R10, R1, R10, 1;
+	SHL R8, R8, 0x4;
+	IMAD R10, R1, 3, R10;
+	IMAD R14, R1, 3, R14;
 	ISETP.LE.AND P0, pt, R2, R0, pt;
-	ST.E [R4], R6;
-	ST.E [R12],R7;
+	ST.E.CG [R4], R6;
+	ST.E.CG [R12],R7;
+	ST.E.CG [R16],R8;
 	@P0 BRA !LOOP
 	IADD R0, R0, R1;
+	IMAD R0, R1, 3, R0;
 	ISETP.EQ.AND P0, pt, R2, R0, pt;
-	@!P0 EXIT;
+	@P0 EXIT;
 
 !Label ENDLOOP
-	ISCADD R1, R1, R4, 1;
-	LD.E R6, [R2];
+
+	IMAD R4, R1, 3, R4;
+
+!Label SUBEND
+	LD.E.CG R6, [R2];
+	IADD R2, R2, R1;
 	SHL R6, R6, 0x4;
-	ST.E [R4], R6;
+	ST.E.CG [R4], R6;
+	IADD R4, R4, R1;
+	ISETP.LT.AND P0, pt, R2, R0, pt;
+	@P0 BRA !SUBEND
 	EXIT;
 
 !EndKernel
